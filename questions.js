008567d20c9235a6eff7dc4796031e18b5b8a718
CMPPro5997 <chirag.patel73@aol.com>
2026-08-17 13:04:28 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,53 @@
+
+const questions = [
+    {
+        question: "Which Indus Valley Civilization site is famous for possessing a prominent artificial brick dockyard?",
+        options: ["Harappa", "Mohenjo-daro", "Lothal", "Kalibangan"],
+        correctIndex: 2
+    },
+    {
+        question: "Who among the following was the founder of the Maurya Empire?",
+        options: ["Ashoka", "Chandragupta Maurya", "Bindusara", "Brihadratha"],
+        correctIndex: 1
+    },
+    {
+        question: "During the reign of which Gupta emperor did the Chinese pilgrim Fa-Hien (Faxian) visit India?",
+        options: ["Chandragupta I", "Samudragupta", "Chandragupta II", "Kumaragupta I"],
+        correctIndex: 2
+    },
+    {
+        question: "In which year took place the First Battle of Panipat, establishing the Mughal Empire in India?",
+        options: ["1526", "1556", "1761", "1530"],
+        correctIndex: 0
+    },
+    {
+        question: "Who was the ruler of the Mughal Dynasty during whose reign the Taj Mahal was built?",
+        options: ["Akbar", "Jahangir", "Shah Jahan", "Aurangzeb"],
+        correctIndex: 2
+    },
+    {
+        question: "Which Indian ruler is widely recognized for forming the Navy of the Maratha Empire?",
+        options: ["Chhatrapati Shivaji Maharaj", "Sambhaji Maharaj", "Baji Rao I", "Balaji Vishwanath"],
+        correctIndex: 0
+    },
+    {
+        question: "In which year did the Battle of Plassey take place, marking the start of British political dominance in Bengal?",
+        options: ["1757", "1764", "1857", "1707"],
+        correctIndex: 0
+    },
+    {
+        question: "Who was the Governor-General of India during the Indian Rebellion of 1857?",
+        options: ["Lord Dalhousie", "Lord Canning", "Lord Curzon", "Lord William Bentinck"],
+        correctIndex: 1
+    },
+    {
+        question: "In which year was the Indian National Congress (INC) founded in Bombay?",
+        options: ["1875", "1885", "1905", "1920"],
+        correctIndex: 1
+    },
+    {
+        question: "Who among the following freedom fighters was popularly known as 'Frontier Gandhi'?",
+        options: ["Subhas Chandra Bose", "Khan Abdul Ghaffar Khan", "Maulana Abul Kalam Azad", "Gopal Krishna Gokhale"],
+        correctIndex: 1
+    }
+];
